c8f0f5bb805fe1ce715f52a9e610521fb1ea54c8
Phaithun <gamebuu002@gmail.com>
2014-10-21 22:11:06 -0700

    Client_Sever_500

---
+++ gen/buusk/example/client_sever_55410500/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package buusk.example.client_sever_55410500;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
